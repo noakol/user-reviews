476c9778df5aefc01abff3bdf8feb61a7b516ff6
noakol <noa.kolodro@amdocs.com>
2018-12-06 08:52:57 +0200

    =server cleanup

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/api/list/add', (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
